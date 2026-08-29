96882ad51162ad3999318562669a1e83b5cc7892
Kartavya Jain <Kartavyajain280506@gmail.com>
2026-08-30 01:05:46 +0530

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from api.upload import router as upload_router
+from api.predict import router as predict_router
 app=FastAPI(
     title="Cortex-AI",
     description="An AI-powered autoML and Data Analysis Platform",
     version="1.0.0"
 )
 app.include_router(upload_router, prefix="/upload", tags=["Upload"])
+app.include_router(predict_router, prefix="/predict", tags=["Predict"])
 @app.get("/")
 def home():
     return{
