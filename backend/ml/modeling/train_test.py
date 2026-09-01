a8ae9b7629533979cfebdbbe913fe8f674b2b72c
Kartavya Jain <Kartavyajain280506@gmail.com>
2026-09-01 22:02:04 +0530

    modified:   backend/ml/modeling/train_test.py 	modified:   backend/ml/saved_models/preprocessing_artifacts.pkl 	modified:   backend/ml/saved_models/scaler.pkl

--- backend/ml/modeling/train_test.py
+++ backend/ml/modeling/train_test.py
@@ -1,11 +1,12 @@
 from sklearn.model_selection import train_test_split as sklearn_train_test_split
 def train_test_split(X, y, test_size=0.2, random_state=42):
+    stratify = y if y.value_counts().min() >= 2 else None
     X_train, X_test, y_train, y_test = sklearn_train_test_split(
         X,
         y,
         test_size=test_size,
         random_state=random_state,
-        stratify=y
+        stratify=stratify
     )
     report = {
         "trest_size": test_size,
